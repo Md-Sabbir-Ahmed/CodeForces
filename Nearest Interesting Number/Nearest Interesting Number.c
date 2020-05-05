67d78f9dd1fcfe0c76b1b4658fd3552e801773ba
Sabbir <sabbir106706@gmail.com>
2020-05-05 17:54:51 +0600

    p2 using c

---
+++ Nearest Interesting Number/Nearest Interesting Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,i,answer=0,temp;
+    scanf("%d",&a);
+    for(i=a;i<=1200;i++){
+    temp=i;
+    int sum=0;
+    while(temp!=0)
+    {
+        int r;
+        r=temp%10;
+        sum=sum+r;
+        temp=temp/10;
+    }
+
+      if(sum%4==0)
+        {
+            answer=i;
+            break;
+        }
+
+    }
+        printf("%d",answer);
+    }
