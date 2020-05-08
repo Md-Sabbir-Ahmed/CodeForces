fb2f15c7e6d5f47284dddc2694e57028417fc7d0
Sabbir <sabbir106706@gmail.com>
2020-05-09 00:51:45 +0600

    p8 using cpp

---
+++ Team/Team.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,x=0;
+    cin>>n;
+    while(n--)
+    {
+      int a,b,c;
+      cin>>a>>b>>c;
+      if(a+b+c>=2)
+      {
+          x++;
+      }
+    }
+     cout<<x<<endl;
+}
+
