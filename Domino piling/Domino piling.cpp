b6bc0a8336ac633cdbf59bad947562483e861111
Sabbir <sabbir106706@gmail.com>
2020-05-26 17:55:14 +0600

    p18 using cpp

---
+++ Domino piling/Domino piling.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int m,n;
+    cin>>m>>n;
+    int k=(m*n)/2;
+    cout<<k;
+    return 0;
+}
+
